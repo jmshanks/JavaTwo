a34b84b496c544e030000ccd590107a3d13dafbf
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-04-10 16:47:32 -0500

    Update CSVReader.java

--- CrimeAnalysis/CSVReader.java
+++ CrimeAnalysis/CSVReader.java
@@ -1,39 +1,42 @@
 package javatwo;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
+
+
 public class CSVReader {
 
-    public static List<Crime> readCrimeFromCSV(String fileName) throws FileNotFoundException {
+
+    public static List<Crime> readCrimeFromCSV(String fileName) throws IOException {
 
         List<Crime> crime = new ArrayList<>();
         Path pathToFile = Paths.get("C://crime2006.csv");
 
 
-        try (BufferedReader br = Files.newBufferedReader(Paths.get("C://crime2006.csv"),
-                StandardCharsets.US_ASCII)) {
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("C://crime2006.csv"))) {
 
             String line = br.readLine();
 
-            while (line != null) {
-                String[] attributes = line.split(",");
-                Crime crimes = listCrime(attributes);
+            for (int i = 1; (line = br.readLine()) != null; i++)
 
-                crime.add(crimes);
+                while (line != null) {
+                    String[] incidents = line.split(",");
+                    Crime crimes = listCrime(incidents);
 
-                line = br.readLine();
-            }
+                    crime.add(crimes);
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+                    line = br.readLine();
+                }
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -42,19 +45,18 @@ public class CSVReader {
     }
 
     private static Crime listCrime(String[] crime) {
-        int date = Integer.parseInt(crime[0]);
+        String date = crime[0];
         String address = crime[1];
         int district = Integer.parseInt(crime[2]);
-        int beat = Integer.parseInt(crime[3]);
-        String grid = crime[4];
+        String beat = crime[3];
+        int grid = Integer.parseInt(crime[4]);
         String crimeDescription = crime[5];
         int ncicCode = Integer.parseInt(crime[6]);
-        int latitude = Integer.parseInt(crime[7]);
-        int longitude = Integer.parseInt(crime[8]);
+        double latitude = Double.parseDouble(crime[7]);
+        double longitude = Double.parseDouble(crime[8]);
 
         return new Crime(date, address, district, beat, grid, crimeDescription, ncicCode, latitude, longitude);
 
     }
 
 }
-
