280a2494fa6b03302d2238fa006222d98ba6effd
jmshanks <46826324+jmshanks@users.noreply.github.com>
2019-04-10 20:26:58 -0500

    Update Crime.java

--- CrimeAnalysis/Crime.java
+++ CrimeAnalysis/Crime.java
@@ -3,7 +3,7 @@ package javatwo;
 import java.io.IOException;
 import java.util.List;
 
-import static javatwo.CSVReader.readCrimeFromCSV;
+import static javatwo.Reader.readCrimeFromCSV;
 
 public class Crime {
 
@@ -18,77 +18,14 @@ public class Crime {
     private double longitude;
 
     public Crime(String date, String address, int district, String beat, int grid, String crimeDescription, int ncicCode, double latitude, double longitude) {
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
         this.date = date;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
         this.address = address;
-    }
-
-    public int getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(int district) {
         this.district = district;
-    }
-
-    public String getBeat() {
-        return beat;
-    }
-
-    public void setBeat(String beat) {
         this.beat = beat;
-    }
-
-    public int getGrid() {
-        return grid;
-    }
-
-    public void setGrid(int grid) {
         this.grid = grid;
-    }
-
-    public String getCrimeDescription() {
-        return crimeDescription;
-    }
-
-    public void setCrimeDescription(String crimeDescription) {
         this.crimeDescription = crimeDescription;
-    }
-
-    public int getNcicCode() {
-        return ncicCode;
-    }
-
-    public void setNcicCode(int ncicCode) {
         this.ncicCode = ncicCode;
-    }
-
-    public double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(double latitude) {
         this.latitude = latitude;
-    }
-
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
@@ -99,10 +36,13 @@ public class Crime {
                 + ", Latitude: " + latitude + ", Longitude: " + longitude + "\n";
     }
 
+
     public static void showCrime() throws IOException {
-        List<Crime> crime = readCrimeFromCSV("C://crime2006.csv");
+        List<Crime> crime = readCrimeFromCSV("D://crime2006.csv");
         for (Crime c : crime) System.out.println(c);
 
     }
+
+
 
 }
